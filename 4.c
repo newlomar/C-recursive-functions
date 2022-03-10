954f804e0a7b1e17266d9194715a3966296a6d9f
Newton Lomar <newtonlomar16@gmail.com>
2022-03-09 22:34:50 -0300

    update on remove 4th recursive function

--- 4.c
+++ 4.c
@@ -8,23 +8,24 @@ struct no {
 	struct no *prox;
 };
 
-ptrno retira_n(ptrno l, int n);
+ptrno retira_n(ptrno *l, int n, ptrno *anterior);
 
 int insere_fim(ptrno * L, int elem);
 
 int main(void) {
 
 	//criando as listas:
-	ptrno lista;
+	ptrno lista = NULL;
 
 	//prenchendo-as
 	for(int i=0; i < 10; i++) {
 		insere_fim(&lista, i);
 	}
 
-	retira_n(lista, 3);
+	retira_n(&lista, 4, NULL);
 	
 	ptrno aux = lista;
+	printf("\n");
 	while (aux != NULL) {
 		printf("%d\n", aux->info);
 		aux = aux->prox;
@@ -32,22 +33,29 @@ int main(void) {
   return 0;
 }
 
-ptrno retira_n(ptrno l, int n) {
-	ptrno aux;
-	if(l->info == n) {
-		
-	}
-	
-	if(l->prox == NULL) {
-		return l;
+ptrno retira_n(ptrno *l, int n, ptrno *anterior) {	
+	if(*l == NULL) {
+		return (*l);
 	}
 	else {
-		return (retira_n(l->prox, n));
+		if((*l)->info == n) {
+			if(!(*anterior == NULL)) {
+				(*anterior)->prox = (*l)->prox;
+			}
+			ptrno apaga = *l;
+			*l = (*l)->prox;
+			free(apaga);
+			return (retira_n((&(*l)), n, (&(*anterior))));
+		}
+		
+		anterior = l;
+		return (retira_n((&(*l)->prox), n, (&(*anterior))));
 	}
 }
 
 int insere_fim(ptrno * L, int elem) { 
 	ptrno novono=(struct no *)malloc(sizeof(struct no));
+	novono->prox = NULL;
 	ptrno aux = *L;
 	if(novono==NULL)
 		return 0;
